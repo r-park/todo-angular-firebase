3f0be5754c00232cb21aaa9bd2686de36e8a91c7
Richard Park <objectiv@gmail.com>
2015-10-04 23:02:05 -0400

    feat(Task): add `createdAt` timestamp

--- src/app/core/task/task.ts
+++ src/app/core/task/task.ts
@@ -1,8 +1,10 @@
 import { Injectable } from 'angular2/angular2';
+import Firebase from 'firebase';
 
 
 export interface ITask {
   completed: boolean;
+  createdAt: any;
   key?: string;
   title: string;
 }
@@ -11,6 +13,7 @@ export interface ITask {
 @Injectable()
 export class Task implements ITask {
   completed: boolean = false;
+  createdAt: any = Firebase.ServerValue.TIMESTAMP;
   title: string;
 
   constructor(title: string = '') {
