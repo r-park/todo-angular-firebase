3fd740a8af0ca4f2be3235ff83eb3e24665c9c4a
Richard Park <objectiv@gmail.com>
2015-11-13 00:59:29 -0500

    fix: `router.dev.min.js` does not exist in alpha.46 bundle

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ var paths = {
   lib: {
     src: [
       'node_modules/angular2/bundles/angular2.min.js',
-      'node_modules/angular2/bundles/router.dev.{min.js,min.js.map}',
+      'node_modules/angular2/bundles/router.dev.js', //{min.js,min.js.map}',
       'node_modules/es6-module-loader/dist/es6-module-loader.{js,js.map}',
       'node_modules/es6-shim/es6-shim.{map,min.js}',
       'node_modules/firebase/lib/firebase-web.js',
