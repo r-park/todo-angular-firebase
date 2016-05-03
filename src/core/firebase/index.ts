8d084076561c70da36a4be730551172d1b48526e
Lander Beeuwsaert <lander.beeuwsaert@gmail.com>
2016-05-03 15:28:44 +0200

    creation of own firebase and google analytics project and referencing to that

--- src/core/firebase/index.ts
+++ src/core/firebase/index.ts
@@ -8,7 +8,8 @@ import {
 
 export const FIREBASE_APP_PROVIDERS: any[] = [
   FIREBASE_PROVIDERS,
-  defaultFirebase('https://ng2-todo-app.firebaseio.com'),
+  //defaultFirebase('https://ng2-todo-app.firebaseio.com'),
+  defaultFirebase('https://blinding-torch-9765.firebaseio.com'),
   firebaseAuthConfig({
     method: AuthMethods.Popup
   })
