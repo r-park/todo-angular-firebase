123caab30b3c1efff275ae30c85bc52255133365
Yannick Koehler <yannick@koehler.name>
2016-10-12 23:25:24 -0400

    Added support for the Anonymous AuthProvider

--- src/auth/components/sign-in.ts
+++ src/auth/components/sign-in.ts
@@ -11,6 +11,7 @@ import { AuthService } from '../services/auth-service';
     <div class="g-row sign-in">
       <div class="g-col">
         <h1 class="sign-in__heading">Sign in</h1>
+        <button class="sign-in__button" (click)="signInAnonymously()" type="button">Anonymously</button>
         <button class="sign-in__button" (click)="signInWithGithub()" type="button">GitHub</button>
         <button class="sign-in__button" (click)="signInWithGoogle()" type="button">Google</button>
         <button class="sign-in__button" (click)="signInWithTwitter()" type="button">Twitter</button>
@@ -21,6 +22,11 @@ import { AuthService } from '../services/auth-service';
 
 export class SignInComponent {
   constructor(private auth: AuthService, private router: Router) {}
+
+  signInAnonymously(): void {
+    this.auth.signInAnonymously()
+      .then(() => this.postSignIn());
+  }
 
   signInWithGithub(): void {
     this.auth.signInWithGithub()
