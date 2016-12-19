fadcc685587f7c8821e3b280883a8604ca47d08e
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-19 11:44:44 -0600

    Moving templates to separate documents

--- src/tasks/components/task-form.ts
+++ src/tasks/components/task-form.ts
@@ -7,20 +7,7 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angul
   styles: [
     require('./task-form.scss')
   ],
-  template: `
-    <form class="task-form" (ngSubmit)="submit()" novalidate>
-      <input
-        [(ngModel)]="title"
-        (keyup.escape)="clear()"
-        autocomplete="off"
-        autofocus
-        class="task-form__input"
-        name="title"
-        placeholder="What needs to be done?"
-        required
-        type="text">
-    </form>
-  `
+  template: require('./task-form.html')
 })
 
 export class TaskFormComponent {
